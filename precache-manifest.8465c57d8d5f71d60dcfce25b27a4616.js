a30a4f2171df68cea98cd811af1c3b8afd223887
kinzey <kinzeyrahardja1@gmail.com>
2021-12-28 22:02:55 +1100

    Updates

---
+++ precache-manifest.8465c57d8d5f71d60dcfce25b27a4616.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b3bd7f4a8da26d350a5",
+    "url": "/robofriends-final/static/css/main.19661495.chunk.css"
+  },
+  {
+    "revision": "2b3bd7f4a8da26d350a5",
+    "url": "/robofriends-final/static/js/main.408cf555.chunk.js"
+  },
+  {
+    "revision": "d52ec001fea358de8cbd",
+    "url": "/robofriends-final/static/js/runtime~main.f944f92c.js"
+  },
+  {
+    "revision": "ad93956398a766816d0c",
+    "url": "/robofriends-final/static/css/2.d2d914e8.chunk.css"
+  },
+  {
+    "revision": "ad93956398a766816d0c",
+    "url": "/robofriends-final/static/js/2.a8ccf58a.chunk.js"
+  },
+  {
+    "revision": "510535223dcd279f3967",
+    "url": "/robofriends-final/static/js/3.e9d442d7.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-final/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "3bdda0348b5d45c80c03921db153a4d8",
+    "url": "/robofriends-final/index.html"
+  }
+];
